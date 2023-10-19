3ef83e396603d82b55396a2fcd67e3677f7e76fa
Elías Maira Manteiga <emairam22@fpcoruna.afundacion.org>
2023-10-19 12:30:48 +0200

    Ejercicio 2.2 tarea1

---
+++ sprint2phpsql/www/mysite/main.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<?php
+	$db = mysqli_connect('localhost','root','1234','mysitedb') or die ('Fail');
+?>
+
+<h1>Conexion establecida</h1>
+</body>
+</html>
---
+++ sprint2sql/mysitedb.sql
@@ -0,0 +1,127 @@
+-- MariaDB dump 10.19  Distrib 10.11.4-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mysitedb
+-- ------------------------------------------------------
+-- Server version	10.11.4-MariaDB-1~deb12u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tComentarios`
+--
+
+DROP TABLE IF EXISTS `tComentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tComentarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comentario` varchar(2000) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `juego_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `juego_id` (`juego_id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `tComentarios_ibfk_1` FOREIGN KEY (`juego_id`) REFERENCES `tJuegos` (`id`),
+  CONSTRAINT `tComentarios_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `tUsuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tComentarios`
+--
+
+LOCK TABLES `tComentarios` WRITE;
+/*!40000 ALTER TABLE `tComentarios` DISABLE KEYS */;
+INSERT INTO `tComentarios` VALUES
+(1,'¡Gran juego!',1,1),
+(2,'Me encanta este juego.',2,2),
+(3,'Buena experiencia de juego.',3,3),
+(4,'Altamente recomendado.',4,4),
+(5,'Excelente trabajo del desarrollador.',5,5);
+/*!40000 ALTER TABLE `tComentarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tJuegos`
+--
+
+DROP TABLE IF EXISTS `tJuegos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tJuegos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `url_imagen` varchar(1000) DEFAULT NULL,
+  `empresa` varchar(100) DEFAULT NULL,
+  `precio` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tJuegos`
+--
+
+LOCK TABLES `tJuegos` WRITE;
+/*!40000 ALTER TABLE `tJuegos` DISABLE KEYS */;
+INSERT INTO `tJuegos` VALUES
+(1,'Tom Clancys Rainbow Six Siege','https://image.api.playstation.com/vulcan/ap/rnd/202210/2019/bhdS9B2OLhzaVDNfVDmd4SO2.png','Ubisoft',29.99),
+(2,'Valorant','https://acf.geeknetic.es/imagenes/auto/2020/6/2/fuh-19183-valorant-salida-publico-muestra.jpg','Riot',0.00),
+(3,'Rocket League','https://www.harrisburgu.edu/wp-content/uploads/189dce017fb19e3ca1b94b2095d519cc514df22c.jpg','Epic Games',0.00),
+(4,'League of Legends','https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2022/8/1/ksfga6rlx2ugfhjd9vnk/league-of-legends','Riot',0.00),
+(5,'7 Days to Die','https://gaming-cdn.com/images/products/1028/616x353/7-days-to-die-pc-mac-juego-steam-cover.jpg?v=1648472552','The Fun Pimps',22.99);
+/*!40000 ALTER TABLE `tJuegos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tUsuarios`
+--
+
+DROP TABLE IF EXISTS `tUsuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tUsuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `contraseña` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tUsuarios`
+--
+
+LOCK TABLES `tUsuarios` WRITE;
+/*!40000 ALTER TABLE `tUsuarios` DISABLE KEYS */;
+INSERT INTO `tUsuarios` VALUES
+(1,'Juan','Pérez','juan@example.com','contrasena123'),
+(2,'María','Rodríguez','maria@example.com','clave123'),
+(3,'Carlos','Gómez','carlos@example.com','contraseña1234'),
+(4,'Laura','Martínez','laura@example.com','seguraclave'),
+(5,'Pedro','López','pedro@example.com','miclavesecreta');
+/*!40000 ALTER TABLE `tUsuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-19 10:49:31
