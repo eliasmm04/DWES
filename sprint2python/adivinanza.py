b3f0381f5aafd3daeff84a69ffd01731be7104b2
Elías Maira Manteiga <emairam22@fpcoruna.afundacion.org>
2023-11-05 20:46:27 +0100

    Ejercicio 6 bueno y corregido

--- sprint2python/adivinanza.py
+++ sprint2python/adivinanza.py
@@ -1,61 +1,45 @@
-print("Adivina adivinanza, Blanca por dentro, verde por fuera. Si quieres que te lo diga, espera.")
-puntuacion = 0
-opciones = {
-'a1': "a: Manzana",
-'b1': "b: Sandía",
-'c1': "c: Pera",
-'op1':'a',
-'op2':'b',
-'op3':'a',
-'a2':'a: Pescado',
-'b2':'b: Pájaro',
-'c2':'c: Plátano',
-'a3':'a: Globo',
-'b3':'b: Cometa',
-'c3':'c: Avión'
-}
+import random
 
-print(opciones['a1'])
-print(opciones['b1'])
-print(opciones['c1'])
+def mostrar_adivinanza(pregunta, opciones, respuesta_correcta):
+    print(pregunta)
+    for opcion in opciones:
+        print(opcion)
+    respuesta_usuario = input("Ingresa tu respuesta (a, b o c): ").lower()
+    if respuesta_usuario == respuesta_correcta:
+        return 10
+    else:
+        return -5
 
-opcionR = input()
 
-if opcionR == opciones['op1']:
-    print("Opción correcta")
-    puntuacion = puntuacion + 10
-else:
-    print("Opción incorrecta")
-    puntuacion = puntuacion + 5
+def main():
+    preguntas = [
+        {
+            'pregunta': "Blanca por dentro, verde por fuera. Si quieres que te lo diga, espera.",
+            'opciones': ['a) Manzana', 'b) Sandía', 'c) Pera'],
+            'respuesta_correcta': 'a'
+        },
+        {
+            'pregunta': "Tiene ojos pero no puede ver. Tiene una pata pero no puede caminar. ¿Qué es?",
+            'opciones': ['a) Pescado', 'b) Pájaro', 'c) Plátano'],
+            'respuesta_correcta': 'b'
+        },
+        {
+            'pregunta': "Aunque me aprietes y me tires al suelo, siempre vuelvo a subir al cielo. ¿Qué soy?",
+            'opciones': ['a) Globo', 'b) Cometa', 'c) Avión'],
+            'respuesta_correcta': 'a'
+        }
+    ]
 
-print('Tiene ojos pero no puede ver. Tiene una pata pero no puede caminar. ¿Qué es?')
 
-print(opciones['a2'])
-print(opciones['b2'])
-print(opciones['c2'])
+    # Selecciona aleatoriamente dos adivinanzas sin repetición
+    adivinanzas_seleccionadas = random.sample(preguntas, 2)
 
-opcionR = input()
+    puntuacion = 0
+    for pregunta in adivinanzas_seleccionadas:
+        puntuacion += mostrar_adivinanza(pregunta["pregunta"], pregunta["opciones"], pregunta["respuesta_correcta"])
 
-if opcionR == opciones['op2']:
-    print("Opción correcta")
-    puntuacion = puntuacion + 10
-else:
-    print("Opción incorrecta")
-    puntuacion = puntuacion + 5
+    print("¡Juego terminado!")
+    print(f"Tu puntuación final es: {puntuacion}")
 
-print("Aunque me aprietes y me tires al suelo, siempre vuelvo a subir al cielo. ¿Qué soy?")
-
-print(opciones['a3'])
-print(opciones['b3'])
-print(opciones['c3'])
-
-opcionR = input()
-
-if opcionR == opciones['op1']:
-    print("Opción correcta")
-    puntuacion = puntuacion + 10
-else:
-    print("Opción incorrecta")
-    puntuacion = puntuacion + 5
-
-print("Puntuación: ",puntuacion)
+if __name__ == "__main__":
+    main()
