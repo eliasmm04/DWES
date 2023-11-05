854f13f73b15aafd7ace4e526334085bba12d85d
Elías Maira Manteiga <emairam22@fpcoruna.afundacion.org>
2023-11-05 19:28:00 +0100

    Ejercicio4 python

---
+++ sprint2python/calculadora.py
@@ -0,0 +1,29 @@
+from ej3 import suma, resta, multiplicacion, division
+
+while True:
+    try:
+        num1 = float(input("Introduce el primer número: "))
+        num2 = float(input("Introduce el segundo número: "))
+    except ValueError:
+        print("Por favor, introduce números válidos.")
+        continue
+    
+    operacion = input("¿Qué operación deseas realizar? (+ para suma, - para resta, * para multiplicación, / para división): ")
+
+    if operacion == '+':
+        resultado = suma(num1, num2)
+    elif operacion == '-':
+        resultado = resta(num1, num2)
+    elif operacion == '*':
+        resultado = multiplicacion(num1, num2)
+    elif operacion == '/':
+        resultado = division(num1, num2)
+    else:
+        print("Operación no válida")
+        continue
+
+    print("El resultado de la operación es: ", resultado)
+
+    continuar = input("¿Quieres hacer más operaciones? (s/n): ")
+    if continuar.lower() != 's':
+        break
