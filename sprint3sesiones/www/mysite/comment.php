71dc74d5abda1baa8463c2543bf2b159d0713fff
“elias” <emairam22@fpcoruna.afundacion.org>
2023-11-16 18:56:12 +0100

    Tarea 3.1 ejercicio 4

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -1,18 +1,25 @@
 <?php
-$db = mysqli_connect ('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+session_start(); // Iniciar la sesión para acceder a $_SESSION
+
+$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+
+$juego_id = $_POST['id'];
+$comentario = $_POST['new_comment'];
+
+// Verificar si el usuario está logueado
+if (isset($_SESSION['user_id'])) {
+    $usuario_id = $_SESSION['user_id'];
+
+    // Insertar el comentario con el usuario_id
+    $query = "INSERT INTO tComentarios (comentario, usuario_id, juego_id, fecha_comentario) VALUES ('$comentario', $usuario_id, $juego_id, CURDATE())";
+    mysqli_query($db, $query) or die('Error al agregar comentario.');
+    
+    echo "<p>Nuevo comentario añadido</p>";
+} else {
+    // Si el usuario no está logueado, puede manejar este caso de otra manera
+    echo "<p>Debes iniciar sesión para añadir un comentario.</p>";
+}
+
+echo "<a href='/detail.php?id=$juego_id'>Volver</a>";
+mysqli_close($db);
 ?>
-<html>
-<body>
-<?php
-    $juego_id = $_POST['id'];
-    $comentario = $_POST['new_comment'];
-    $query = "INSERT INTO tComentarios (comentario, usuario_id, juego_id,fecha_comentario) VALUES ('" .$comentario. "' ,NULL, " . $juego_id . ",CURDATE())" ;
-    mysqli_query ($db, $query) or die('Error');
-    echo "<p>Nuevo comentario ";
-    echo mysqli_insert_id($db) ;
-    echo " añadido</p>";
-    echo "<a href='/detail.php?id=". $juego_id . "'>Volver</a>";
-    mysqli_close ($db) ;
-?>
-</body>
-</html>
