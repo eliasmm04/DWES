2133c444adb4e18453ae18d0591b6c03d07afa29
“elias” <emairam22@fpcoruna.afundacion.org>
2023-11-29 23:14:53 +0100

    2 end point

--- sprint4django/RestApi/webserviceapp/views.py
+++ sprint4django/RestApi/webserviceapp/views.py
@@ -18,3 +18,20 @@ def devolver_juegos(request):
 	return JsonResponse(respuesta_final,safe=False)
 
 # Create your views here.
+
+def devolver_juegos_por_id(request, id_solicitado):
+	juegos = Tjuegos.objects.get (id = id_solicitado)
+	comentarios = juegos.tcomentarios_set.all()
+	lista_comentarios = []
+	for fila_comentario_sql in comentarios:
+		diccionario = {}
+		diccionario['id'] = fila_comentario_sql.id
+		
+	resultado = {
+		'id': juegos.id,
+		'nombre': juegos.nombre,
+		'precio': juegos.precio,
+		
+	}
+	return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False})
+
